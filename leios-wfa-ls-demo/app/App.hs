d75cbe0f7b60c8b3990e972b238a61b70e549a01
perturbing <thomas.vellekoop@iohk.io>
2026-02-03 11:13:16 +0100

    add basic seats and fix deps to work with latest cardano-base

--- leios-wfa-ls-demo/app/App.hs
+++ leios-wfa-ls-demo/app/App.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
 
+import Cardano.Crypto.DSIGN
+import Cardano.Crypto.Seed
+import Cardano.Crypto.Util (SignableRepresentation (..))
 import Cardano.Query
 import Cardano.WeightedFaitAccompli
+import qualified Data.ByteString as BS
 import Data.List (sortOn)
 import qualified Data.Map.Strict as Map
 import Data.Ord (Down (..))
+import Data.Proxy (Proxy (..))
+import Data.Word (Word16)
 import Options.Applicative
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
@@ -43,4 +50,17 @@ main = do
     eMap <- queryPoolDistrMap localNodeConnInfo
     case eMap of
         Left err -> hPutStrLn stderr (renderQueryError err) >> exitFailure
-        Right m -> print $ findIStar 1000 (Map.toList m)
+        Right m -> do
+            let pools = mkOrderedSetOfParties $ Map.toList m
+                committeeSize = 1000 :: CommitteeSize
+            print $ last $ appointSeats committeeSize pools
+
+    let seedLen = fromIntegral (seedSizeDSIGN (Proxy @BLS12381MinVerKeyDSIGN))
+        seedBytes = BS.replicate seedLen 42
+        seed = mkSeedFromBytes seedBytes
+        skVer = genKeyDSIGN @BLS12381MinVerKeyDSIGN seed
+        vkVer = deriveVerKeyDSIGN @BLS12381MinVerKeyDSIGN skVer
+        simpleMsg = "Hello World" :: BS.ByteString
+        emptyBLSContext = BLS12381SignContext Nothing Nothing
+        sigVer = signDSIGN @BLS12381MinVerKeyDSIGN emptyBLSContext simpleMsg skVer
+    print $ verifyDSIGN @BLS12381MinVerKeyDSIGN emptyBLSContext vkVer simpleMsg sigVer
